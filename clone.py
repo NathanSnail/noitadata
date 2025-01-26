5b5b2a1be0af658b6d177576f126955b2323c5b7
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-01-27 03:00:32 +1300

    fix bug with deleted content and new update

--- clone.py
+++ clone.py
@@ -1,27 +1,32 @@
 import shutil
 
+shutil.rmtree("data")
 shutil.copytree(
-	"/home/nathan/.local/share/Steam/steamapps/compatdata/881100/pfx/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/data",
-	"data",
-	dirs_exist_ok=True,
+    "/home/nathan/.local/share/Steam/steamapps/compatdata/881100/pfx/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/data",
+    "data",
+    dirs_exist_ok=True,
 )
 shutil.copytree(
-	"/home/nathan/.local/share/Steam/steamapps/common/Noita/data",
-	"data",
-	dirs_exist_ok=True,
+    "/home/nathan/.local/share/Steam/steamapps/common/Noita/data",
+    "data",
+    dirs_exist_ok=True,
 )
+shutil.rmtree("example_mod")
 shutil.copytree(
-	"/home/nathan/.local/share/Steam/steamapps/common/Noita/mods/example/",
-	"example_mod",
-	dirs_exist_ok=True,
+    "/home/nathan/.local/share/Steam/steamapps/common/Noita/mods/example/",
+    "example_mod",
+    dirs_exist_ok=True,
 )
+shutil.rmtree("example_mod")
 shutil.copytree(
-	"/home/nathan/.local/share/Steam/steamapps/common/Noita/mods/nightmare/",
-	"nightmare_mod",
-	dirs_exist_ok=True,
+    "/home/nathan/.local/share/Steam/steamapps/common/Noita/mods/nightmare/",
+    "nightmare_mod",
+    dirs_exist_ok=True,
 )
+shutil.rmtree("nightmare_mod")
 shutil.copytree(
-	"/home/nathan/.local/share/Steam/steamapps/common/Noita/mods/daily_practice/",
-	"daily_practice_mod",
-	dirs_exist_ok=True,
+    "/home/nathan/.local/share/Steam/steamapps/common/Noita/mods/daily_practice/",
+    "daily_practice_mod",
+    dirs_exist_ok=True,
 )
+shutil.rmtree("daily_practice_mod")
